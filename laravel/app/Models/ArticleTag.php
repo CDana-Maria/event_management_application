c713fa46a7ec8a06af7f7d8f1dc59bb9151e7c34
dana-maria <dana-maria@asus.d-m>
2021-05-12 11:32:38 +0300

    Create article model

---
+++ laravel/app/Models/ArticleTag.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArticleTag extends Model
+{
+    use HasFactory;
+
+    protected $table = 'article_tag';
+
+    public $timestamps = false;
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
+    public function tags()
+    {
+        return $this->hasMany(Tag::class);
+    }
+}
